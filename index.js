e609d88b6f02736f280a53b5bd05be1d4fbc9e0f
Lolita <lolita.danancher@wanadoo.fr>
2022-09-17 23:26:02 +0200

    simplification js

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-let red;
-let green;
-let blue;
-
 setInterval(colorChange = () => {
-    red = Math.floor(Math.random() * 255);
-    green = Math.floor(Math.random() * 255);
-    blue = Math.floor(Math.random() * 255);
-    color.textContent = `rgb(${red},${green},${blue})`;
-    document.body.style.backgroundColor = `rgb(${red},${green},${blue})`
-},3500)
+    const red = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255);
+    const blue = Math.floor(Math.random() * 255);
+    
+    const color = `rgb(${red},${green},${blue})`
+    document.body.innerHTML = `<h2> ${color}</h2>`;
+    document.body.style.backgroundColor = color
+},1000)
